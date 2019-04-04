f8984d00709fbf2a8f617ac60de4371a29be3992
Onur <onur.sazak@hotmail.com.tr>
2019-04-04 03:24:03 +0200

    1- JSTL (JSP Standard Tag Library)

---
+++ DemoApp/src/DemoServlet.java
@@ -0,0 +1,30 @@
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/DemoServlet")
+public class DemoServlet extends HttpServlet
+{
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String name = "Navin";
+		
+		request.setAttribute("label1", name);
+		
+		
+		//calling jsp file from servlet file.
+		RequestDispatcher rd = request.getRequestDispatcher("display.jsp");
+		rd.forward(request, response);
+		
+		 
+		
+	}
+}
